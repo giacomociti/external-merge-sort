afde7051e35da8979a9c45db25289dfade7c5f47
Giacomo Citi <giacomo.citi@zazuko.com>
2023-11-21 15:13:19 +0100

    force remove temp dir

--- storage.js
+++ storage.js
@@ -23,7 +23,7 @@ export const createStore = (write, extension = '') => {
     },
     dispose: async () => {
       if (temp) {
-        fs.rmSync(temp, { recursive: true })
+        fs.rmSync(temp, { recursive: true, force: true })
       }
     }
   }
